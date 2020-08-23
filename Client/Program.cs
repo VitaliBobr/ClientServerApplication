91365e8ebdb19b8b7ccf87181d11e089774fe181
Vitali <wiandrustudio@gmail.com>
2020-08-23 19:12:52 +0300

    First commit

---
+++ Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class MainClass
+    {
+        static Socket socket = new Socket(  AddressFamily.InterNetwork, 
+                                            SocketType.Stream, 
+                                            ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CLIENT V2");
+            socket.Connect("192.168.1.3", 904);
+            string message = "ahah";
+            byte[] buffer = Encoding.ASCII.GetBytes(message.ToCharArray());
+            socket.Send(buffer);
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    class MainClass
+    {
+        static Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            socket.Bind(new IPEndPoint(IPAddress.Any, 3389));
+            socket.Listen(10);
+            Socket connecter = socket.Accept();
+            byte[] buffer = new byte[1024];
+            connecter.Receive(buffer);
+            Console.WriteLine(Encoding.ASCII.GetString(buffer));
+        }
+    }
+}
